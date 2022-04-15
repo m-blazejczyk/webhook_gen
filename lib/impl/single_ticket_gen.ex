18864f203ad5e33879b18390aa267afe0c9dd037
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-04-14 23:00:57 -0400

    Refactored and added the 'sink' argument

--- lib/impl/single_ticket_gen.ex
+++ lib/impl/single_ticket_gen.ex
@@ -1,19 +1,32 @@
 defmodule WebhookGen.Impl.SingleTicketGen do
-  @type t :: {sleep_minutes :: integer(), payload :: String.t()}
+  @type args :: %{time_scale: integer(), event_sink: pid()}
+  @type e :: {sleep_minutes :: integer(), payload :: any()}
 
-  @spec start(list(t()), integer) :: pid | {pid, reference}
-  def start(events, time_scale) do
-    Process.spawn(__MODULE__, :generate, [events, time_scale], [])
+  @spec start(args(), list(e())) :: pid | {pid, reference}
+  def start(args, events) do
+    Process.spawn(__MODULE__, :generate, [args, events], [])
   end
 
-  @spec generate(list(t()), integer) :: true
-  def generate([{sleep, payload} | remaining_events], time_scale) do
+  @spec generate(args(), list(e())) :: true
+  def generate(
+        args = %{time_scale: time_scale, event_sink: sink},
+        [{sleep, payload} | remaining_events]
+      ) do
     Process.sleep(div(sleep * 1000 * 60, time_scale))
-    IO.puts(payload)
-    generate(remaining_events, time_scale)
+    send(sink, {:webhook_event, payload})
+    generate(args, remaining_events)
   end
 
-  def generate([], _time_scale) do
+  def generate(_args, []) do
     Process.exit(self(), {:shutdown, :ticket_closed})
+  end
+
+  # Included only for testing purposes
+  def sink_fun() do
+    receive do
+      {:webhook_event, payload} ->
+        IO.puts(payload)
+        sink_fun()
+    end
   end
 end
